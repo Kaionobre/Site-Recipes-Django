0dc06e30208c06eb9ee30f86a5970c64a53adf3e
Kaionobre <kaionobrega00@gmail.com>
2024-02-22 16:25:11 -0300

    Deixando o site mais dinâmico, adicionando fotos e nomes randons

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
 ]
